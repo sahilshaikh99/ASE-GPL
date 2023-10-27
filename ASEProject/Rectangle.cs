6a05ff7fa70d520372825e15b3dfcf9d6f4d7b75
Sahil Shaikh <sahilshaikh8264@gmail.com>
2023-10-28 00:01:37 +0530

    Rectangle.cs Class added to draw rectangle using shape base class draw method

---
+++ ASEProject/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using ASEProject;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASEProject
+{
+    internal class Rectangle: Shape
+    {
+        public override void Draw(Graphics graphics, int x, int y)
+        {
+            using (Brush brush = new SolidBrush(Color.Red))
+            {
+                graphics.FillRectangle(brush, x, y, 150, 100);
+            }
+        }
+    }
+}

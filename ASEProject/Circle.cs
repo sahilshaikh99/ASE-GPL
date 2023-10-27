706cf8f04ffc3dc8b6b5b501f160de5c031215f6
Sahil Shaikh <sahilshaikh8264@gmail.com>
2023-10-27 23:27:10 +0530

    Circle Shape class created to draw circle which inherits shape base class

---
+++ ASEProject/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASEProject
+{
+    internal class Circle: Shape
+    {
+        public override void Draw(Graphics graphics, int x, int y)
+        {
+            using (Brush brush = new SolidBrush(Color.Blue))
+            {
+                graphics.FillEllipse(brush, x, y, 100, 100);
+            }
+        }
+    }
+}

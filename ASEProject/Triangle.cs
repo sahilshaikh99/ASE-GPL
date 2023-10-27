8691538fc96efddb5c0735bfde2cde3cf90e8696
Sahil Shaikh <sahilshaikh8264@gmail.com>
2023-10-28 00:04:33 +0530

    Triangle.cs Class added to draw triangle using shape base class draw method

---
+++ ASEProject/Triangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASEProject
+{
+    internal class Triangle: Shape
+    {
+        public override void Draw(Graphics graphics, int x, int y)
+        {
+            Point[] points = { new Point(x, y), new Point(x + 100, y), new Point(x + 50, y + 100) };
+            using (Brush brush = new SolidBrush(Color.Red))
+            {
+                graphics.FillPolygon(brush, points);
+            }
+        }
+
+    }
+}

0cccc6c9f98cb8356b98775a9a527bdb056462fa
Sahil Shaikh <sahilshaikh8264@gmail.com>
2023-11-09 23:42:58 +0530

    Added Pen Color test case

---
+++ ASEProjectTest/DrawHandlerTest.cs
@@ -0,0 +1,36 @@
+﻿using ASEProject;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASEProjectTest
+{
+    [TestClass]
+    public class DrawHandlerTest
+    {
+        [TestMethod]
+        public void ExecutePenColorCommand_ValidColor_PenColorUpdated()
+        {
+            DrawHandler drawHandler = new DrawHandler(800, 600);
+            string penColorCommand = "pen blue";
+
+            drawHandler.ExecuteCommand(penColorCommand);
+            Color penColor = drawHandler.GetPenColor();
+
+            Assert.AreEqual(Color.Blue, penColor);
+         }
+
+        [TestMethod]
+        public void SetPenColor_InvalidColor_CommandHandlerException()
+        {
+            var drawHandler = new DrawHandler(800, 600);
+            string invalidColorName = "invalidcolor";
+
+            Assert.ThrowsException<ArgumentException>(() => drawHandler.SetPenColor(invalidColorName));
+        }
+
+    }
+}

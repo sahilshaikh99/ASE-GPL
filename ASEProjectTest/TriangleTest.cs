a019bd552f3ee498bc76bc958e4f3f75eb75cce6
Sahil Shaikh <sahilshaikh8264@gmail.com>
2023-11-13 03:32:42 +0530

    Added Triangle Test cases

---
+++ ASEProjectTest/TriangleTest.cs
@@ -0,0 +1,62 @@
+﻿using ASEProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+
+namespace ASEProjectTest
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        [TestMethod]
+        public void TestDrawTriangle_ValidParameters()
+        {
+            Triangle triangle = new Triangle();
+            Bitmap bitmap = new Bitmap(800, 600);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            Color penColor = Color.Black;
+            int x = 100;
+            int y = 100;
+            int width = 80;
+            int height = 60;
+            int radius = 30;
+            bool fill = true;
+
+            triangle.Draw(graphics, penColor, x, y, width, height, radius, fill);
+
+            Color expectedColor = Color.FromArgb(255, 0, 0, 0);
+
+            Color pixelColor = bitmap.GetPixel(x, y);
+
+            Assert.AreEqual(expectedColor, pixelColor);
+        }
+
+        [TestMethod]
+        public void TestDrawTriangle_InvalidDimensions_ThrowsArgumentException()
+        {
+            CommandParser parser = new CommandParser();
+
+            string inputCommand = "triangle -10 40 30";
+
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                var (shapeName, x, y, width, height, radius, penColorName, fill) = parser.ParseCommand(inputCommand, 858, 477);
+            });
+        }
+
+        [TestMethod]
+        public void TestDrawTriangle_MissingParameter_ThrowsArgumentException()
+        {
+            CommandParser parser = new CommandParser();
+
+            string inputCommand = "triangle -10 40";
+
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                var (shapeName, x, y, width, height, radius, penColorName, fill) = parser.ParseCommand(inputCommand, 858, 477);
+            });
+        }
+    }
+
+}

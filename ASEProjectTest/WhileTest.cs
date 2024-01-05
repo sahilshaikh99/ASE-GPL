b871cf6bd376ec4c64d1fae40fc52956d0439d87
Sahil Shaikh <sahilshaikh8264@gmail.com>
2024-01-05 23:36:33 +0530

    Added While Loop Test cases

---
+++ ASEProjectTest/WhileTest.cs
@@ -0,0 +1,80 @@
+﻿using ASEProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASEProjectTest
+{
+        [TestClass]
+        public class WhileHandlerTests
+        {
+            [TestMethod]
+            public void TestHandleWhileLoop_NumericComparison_True()
+            {
+                VariableManager variableManager = new VariableManager();
+                WhileHandler whileHandler = new WhileHandler(variableManager);
+                variableManager.SetVariableValue("variableA", 3);
+
+                bool result = whileHandler.HandleWhileLoop("variableA < 5");
+
+                Assert.IsTrue(result);
+            }
+
+            [TestMethod]
+            public void TestHandleWhileLoop_NumericComparison_False()
+            {
+                VariableManager variableManager = new VariableManager();
+                WhileHandler whileHandler = new WhileHandler(variableManager);
+                variableManager.SetVariableValue("variableA", 7);
+
+                bool result = whileHandler.HandleWhileLoop("variableA < 5");
+
+                Assert.IsFalse(result);
+            }
+
+            [TestMethod]
+            public void TestHandleWhileLoop_InvalidCondition()
+            {
+                VariableManager variableManager = new VariableManager();
+                WhileHandler whileHandler = new WhileHandler(variableManager);
+
+                Assert.ThrowsException<ArgumentException>(() => whileHandler.HandleWhileLoop("invalid condition"));
+            }
+
+            [TestMethod]
+            public void TestEvaluateCondition_NumericComparison_True()
+            {
+                
+                VariableManager variableManager = new VariableManager();
+                WhileHandler whileHandler = new WhileHandler(variableManager);
+                variableManager.SetVariableValue("variableA", 3);
+
+                bool result = whileHandler.EvaluateCondition("variableA < 5");
+
+                Assert.IsTrue(result);
+            }
+
+            [TestMethod]
+            public void TestEvaluateCondition_NumericComparison_False()
+            {
+                VariableManager variableManager = new VariableManager();
+                WhileHandler whileHandler = new WhileHandler(variableManager);
+                variableManager.SetVariableValue("variableA", 7);
+
+                bool result = whileHandler.EvaluateCondition("variableA < 5");
+
+                Assert.IsFalse(result);
+            }
+
+            [TestMethod]
+            public void TestEvaluateCondition_InvalidCondition()
+            {
+                VariableManager variableManager = new VariableManager();
+                WhileHandler whileHandler = new WhileHandler(variableManager);
+
+                Assert.ThrowsException<ArgumentException>(() => whileHandler.EvaluateCondition("invalid condition"));
+            }
+        }
+    }
